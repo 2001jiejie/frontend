9d5676df8cc5e8f522e21b250420a614a6d85f26
2001jiejie <174898972+2001jiejie@users.noreply.github.com>
2024-11-24 19:17:11 +0800

    功能完善

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import { getToken } from '@/utils/auth'
+import { getToken } from "@/utils/auth";
 
 const routes = [
   {
@@ -26,14 +26,14 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  const token = getToken()
-  
+  const token = getToken();
+
   if (to.meta.requiresAuth && !token) {
     // 需要登录但未登录，跳转到登录页
-    next('/login')
+    next("/login");
   } else {
-    next()
+    next();
   }
-})
+});
 
 export default router;
